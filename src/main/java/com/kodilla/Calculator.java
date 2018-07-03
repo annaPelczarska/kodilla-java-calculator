bfec7dcd448064a39cb91e43fd90ddb3ecb75b22
anna.Pelczarska <anna.pelczrska@gmail.com>
2018-07-03 17:13:40 +0200

    Kalkulator

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla;
+
+public class Calculator {
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractBFromA(int a, int b) {
+        return a - b;
+    }
+}
